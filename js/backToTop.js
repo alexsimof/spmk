3c4415ed11e5b00a450c40e88f969c95b9f8495c
Alex Simonov <alex.simonof@gmail.com>
2023-02-21 22:41:26 +0200

    first

---
+++ js/backToTop.js
@@ -0,0 +1,34 @@
+
+const btnUp = {
+  el: document.querySelector('.back__top'),
+  
+  show() {
+    this.el.classList.remove('back-to-top_hide');
+  },
+
+  hide() {
+    this.el.classList.add('back-to-top_hide');
+  },
+
+  addEventListener() {
+
+    window.addEventListener('scroll', () => {
+
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+      scrollY > 400 ? this.show() : this.hide();
+    });
+
+    document.querySelector('.back__top').onclick = () => {
+
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+
+    }
+  }
+}
+
+btnUp.addEventListener();
