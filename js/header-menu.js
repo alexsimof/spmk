b708c714b4c495f0f502d1d54d2005bf0de0fb11
Alex Simonov <alex.simonof@gmail.com>
2023-03-13 10:26:24 +0200

    1

--- js/header-menu.js
+++ js/header-menu.js
@@ -6,6 +6,7 @@ button.addEventListener('click', (e) => {
   e.preventDefault();
   let activ = document.querySelector(".menu__toggle"); // находим блок с меню
   let showMenu = document.querySelector(".nav-mobile"); // находим блок с меню
+  
   activ.classList.toggle('activ') // присваеваем класс чтоб показать
   button.classList.toggle('color') // присваеваем класс чтоб показать
   showMenu.classList.toggle('show-menu') // присваеваем класс чтоб показать
