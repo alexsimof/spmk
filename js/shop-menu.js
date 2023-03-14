8e5e13422833ca6f2434ae6ca067d3ac7256c4f2
Alex Simonov <alex.simonof@gmail.com>
2023-03-14 21:31:21 +0200

    1

--- js/shop-menu.js
+++ js/shop-menu.js
@@ -1,10 +1,23 @@
 
 
-let btn = document.querySelector(".main-nav__link");
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  let show = document.querySelector(".main-nav-menu");
-  show.classList.toggle('show')
+const btnMenu = document.querySelector(".main-nav__link");
+const menu = document.querySelector(".main-nav-menu");
+const toggleMenu = function () {
+    menu.classList.toggle("show");
+}
+
+btnMenu.addEventListener("click", function (e) {
+    e.preventDefault();
+    toggleMenu();
 });
 
+document.addEventListener("click", function (e) {
+    const target = e.target;
+    const its_menu = target == menu || menu.contains(target);
+    const its_btnMenu = target == btnMenu;
+    const menu_is_active = menu.classList.contains("show");
 
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+        toggleMenu();
+    }
+});
