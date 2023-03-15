163054ce3cb3f40378bbd7a1f95aabdbfbb3c990
Alex Simonov <alex.simonof@gmail.com>
2023-03-15 14:50:38 +0200

    2

--- js/map.js
+++ js/map.js
@@ -6,12 +6,43 @@ function init() {
       {
         center: [55.76, 37.64],
         zoom: 8,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      },
+        },
+        {
+          searchControlProvider: "yandex#search",
+        },
       
-    ),
+      ),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+
+      myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+            hintContent: 'Собственный значок метки с контентом',
+            balloonContent: 'А эта — новогодняя',
+            iconContent: '12'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'spmk#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/point.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    
+
+
     objectManager = new ymaps.ObjectManager({
       // Чтобы метки начали кластеризоваться, выставляем опцию.
       clusterize: true,
@@ -20,18 +51,25 @@ function init() {
       clusterDisableClickZoom: true,
     });
 
-    myMap.behaviors.disable('scrollZoom')
+    myMap.behaviors.disable('scrollZoom');
 
 
 
-  // Чтобы задать опции одиночным объектам и кластерам,
-  // обратимся к дочерним коллекциям ObjectManager.
-  objectManager.objects.options.set("preset", "islands#greenDotIcon");
-  objectManager.clusters.options.set("preset", "islands#greenClusterIcons");
-  myMap.geoObjects.add(objectManager);
-  $.ajax({
-    url: "/js/data.json",
-  }).done(function (data) {
-    objectManager.add(data);
-  });
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    objectManager.objects.options.set("preset", "islands#greenDotIcon");
+    objectManager.clusters.options.set("preset", "islands#greenClusterIcons");
+    myMap.geoObjects.add(objectManager);
+    $.ajax({
+      url: "/js/data.json",
+    }).done(function (data) {
+      objectManager.add(data);
+    });
+
+
+
+
+  myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
 }
--- js/video.js
+++ js/video.js
@@ -1,16 +1,20 @@
 
 const videoBtn = document.querySelector(".video__button");
 const videoImg = document.querySelector(".video__img");
+const video = document.querySelector(".video");
 videoBtn.addEventListener("click", (e) => {
   e.preventDefault();
   videoBtn.className = "video-active";
-  const iframe = document.createElement("iframe");
-  videoBtn.append(iframe);
-  iframe.className = "video__frame";
-  let videoSrc = videoBtn.href;
-  let videoId = videoSrc.substr(17) + "?autoplay=1&mute=1";
+  const videoPlay = document.createElement("video");
+  const source = document.createElement("source");
+  video.append(videoPlay);
+  videoPlay.append(source);
+  videoPlay.className = "video__frame";
   videoImg.style.display = "none";
-  let link = "https://www.youtube.com/embed/" + videoId;
-  iframe.src = link;
-  iframe.style.display = "block";
+  source.src = './video/SPMC_V.mp4';
+  videoPlay.style.display = "block";
+  videoPlay.setAttribute("autoplay", true)
+  videoPlay.setAttribute("controls", true)
+  videoPlay.muted =  "muted";
+  source.setAttribute("type",'video/mp4')
 });
